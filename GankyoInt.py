4bd1660b6c08f0df172c0a4df0fbb3c9eaf3456e
FussionPK <142238260+FussionPK@users.noreply.github.com>
2024-10-16 09:25:52 +0200

    YOH!

---
+++ GankyoInt.py
@@ -0,0 +1,26 @@
+#Gankyo Threat Intelligence Platform -- GTIP
+
+#Welcome to the platform mr user
+
+#Below is the database.
+
+Database = []
+
+#Declaring variables
+
+Threat = input()
+
+#function that takes the input of a user and will keep asking for input until you type stop and if the input matches the database a message is printed and if it doesnt another message is printed and the input is then inserted into the database
+
+def shelby ():
+    while Threat in Database:
+
+        print(f"Threat matches signature... providing information about {Threat})
+        
+        if Threat != Database:
+            print("Threat information not found") and Database.append(Threat)
+        elif:
+            Threat == "Stop"
+            break:
+    
+shelby()
